aed4a00f8039852b49127a44dcaa2a910abb31cd
zk-phi <zk-phi@users.noreply.github.com>
2024-05-17 02:46:18 +0900

    Initial commit

---
+++ Readme.markdown
@@ -0,0 +1,57 @@
+# shackle-pop
+
+WIP warning: This plugin may have some bugs.
+
+Make shackle.el behave more like popwin.el.
+
+## Installation
+
+Just load this package.
+
+``` emacs-lisp
+(require 'shackle-pop)
+```
+
+## Features / Configurations
+
+All these features are enabled by default. If you want to disable some of them, set to `nil`.
+
+- `shackle-pop-auto-close`
+
+  When a window is popped up by a shackle, the window will be auto-closed after `keyboard-quit
+  (C-g)` is pressed.
+
+- `shackle-pop-auto-reuse`
+
+  When a window is popped up by shackle, the window will be reused when another popup is requested.
+
+- `shackle-pop-auto-align`
+
+  When a window is popped up by shackle, the window is automatically aligned even if the rule does
+  not contain `:align t`.
+
+  This feature exists just to clean-up `shackle-rule`.
+
+  You may also want to set `shackle-default-alignment` and `shackle-default-size`.
+
+## Sample configuration
+
+``` emacs-lisp
+(require 'shackle)
+(require 'shackle-pop)
+
+;; we don't need to specify (:align 'below :size 0.33) for each entries
+(setq shackle-rules '(("*Help*"                 :select t   :popup t)
+                      ("*info*"                 :select t   :popup t)
+                      ("*Warnings*"             :select t   :popup t)
+                      ("*Buffer List*"          :select t   :popup t)
+                      ("*compilation*"          :select nil :popup t)
+                      ("*Shell Command Output*" :select t   :popup t)
+                      ;; when selected compilation may fail ?
+                      ("*Compile-Log*"          :select nil :popup t)
+                      ("*Backtrace*"            :select t   :popup t)
+                      ("*Completions*"          :select nil :popup t))
+      shackle-default-rule '(:same t)
+      shackle-default-alignment 'below
+      shackle-default-size 0.33)
+```
---
+++ shackle-pop.el
@@ -0,0 +1,67 @@
+(require 'shackle)
+;; shackle.el also requires cl-lib, so this makes it no slower to load
+(require 'cl-lib)
+
+(defvar shackle-pop-auto-close t
+  "When non-nil, popup windows are automatically closed after
+`keyboard-quit' is requested.")
+
+(defvar shackle-pop-auto-reuse t
+  "When non-nil and if a popup window is already shown, do not
+create another but just reuse it.")
+
+(defvar shackle-pop-close-timer-delay 0.05
+  "Delay for the idle timer to monitor `keyboard-quit'.")
+
+(defvar shackle-pop-auto-align t
+  "When non-nil, popups will be aligned even when (:align t) is not
+specified in the rule.")
+
+(defvar shackle-pop--timer-object nil)
+(defvar shackle-pop--windows nil)
+
+(defun shackle-pop--maybe-start-timer ()
+  (unless shackle-pop--timer-object
+    (setq shackle-pop--timer-object
+          (run-with-idle-timer
+           shackle-pop-close-timer-delay
+           shackle-pop-close-timer-delay
+           (lambda ()
+             (when (eq last-command 'keyboard-quit)
+               (dolist (wnd shackle-pop--windows)
+                 (and (window-live-p wnd) (delete-window wnd)))
+               (setq shackle-pop--windows nil))
+             (unless shackle-pop--windows
+               (cancel-timer shackle-pop--timer-object)
+               (setq shackle-pop--timer-object nil)))))))
+
+(defun shackle-pop--maybe-clear-windows ()
+  (when (= (length (window-list)) 1)
+    (setq shackle-pop--windows nil)))
+(add-hook 'window-configuration-change-hook 'shackle-pop--maybe-clear-windows)
+
+(define-advice shackle-display-buffer (:around (fn buffer alist plist))
+  (let* ((popup-p (plist-get plist :popup))
+         (reused-window
+          (and shackle-pop-auto-reuse
+               popup-p
+               shackle-pop--windows
+               (cl-some (lambda (w) (and (window-live-p w) w)) shackle-pop--windows))))
+    (cond (reused-window
+           (set-window-buffer reused-window buffer)
+           (when (plist-get plist :select)
+             (select-window reused-window))
+           reused-window)
+          (t
+           (let* ((plist (nconc plist `(:align ,shackle-pop-auto-align)))
+                  (wnd (funcall fn buffer alist plist)))
+             (when popup-p
+               (push wnd shackle-pop--windows))
+             (when shackle-pop-auto-close
+               (shackle-pop--maybe-start-timer))
+             wnd)))))
+
+(defun shackle-pop-popup-buffer (buffer)
+  (shackle-display-buffer buffer nil '(:select t :popup t)))
+
+(provide 'shackle-pop)
